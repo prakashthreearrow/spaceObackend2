04f58cdb46c8809e90a7d10b97cf511ecf4df5cf
prakash11s <psahu5981@gmail.com>
2024-01-11 11:30:06 +0400

    chnages

--- src/controllers/api/ContactBook.js
+++ src/controllers/api/ContactBook.js
@@ -155,7 +155,6 @@ module.exports = {
                             }
                         });
 
-                        
                         if (existingEmails.length === 0) {
                             await User.update(
                                 { name: requestParams.name },
